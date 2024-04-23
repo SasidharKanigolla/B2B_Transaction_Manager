507995c59d2305ed5b17f6d23374dc833f9ecd1f
sasi <sasidharkanigolla44@gmail.com>
2024-04-23 14:44:16 +0530

    Fixed Some bugs

--- Frontend/my-app/src/utils/Constants.js
+++ Frontend/my-app/src/utils/Constants.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:8080";
-export const DELETEPASS = "Sasidhar";
+export const URL = "https://b2b-transaction-manager.onrender.com";
+// export const DELETEPASS = "Sasidhar";
